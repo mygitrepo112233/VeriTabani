79a085f63d86bf78bae2d2706fa7ce0cdfe7c531
Serhat Har <serhatrah83@gmail.com>
2025-02-14 19:46:02 +0300

    .

--- Odev/Controllers/WeatherForecastController.cs
+++ Odev/Controllers/WeatherForecastController.cs
@@ -5,11 +5,7 @@ namespace Odev.Controllers {
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
+        
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger) {
@@ -21,7 +17,6 @@ namespace Odev.Controllers {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
